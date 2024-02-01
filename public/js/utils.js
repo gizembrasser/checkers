dddc89a0377f387a3250c380577db8f569f56aed
gizembrasser <gizembrasser101@gmail.com>
2024-02-01 18:51:04 +0100

    refactor

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,28 @@
+// Function that returns all diagonally adjacent squares in front of a certain starting square
+function checkDiagonals(start) {
+    const row = Math.floor((63 - start) / width) + 1;
+    const col = start % width;
+
+    // Get the id for the diagonally adjacent squares, store it in the array
+    const diagonalSquares = [];
+
+    // Check top-left
+    if (row > 1 && col > 0) {
+        diagonalSquares.push((width - (row - 1)) * width + col - 1);
+    }
+
+    // Check top-right
+    if (row > 1 && col < 7) {
+        diagonalSquares.push((width - (row - 1)) * width + col + 1);
+    }
+
+    return diagonalSquares;
+};
+
+
 // Function to check if the move is 1 square diagonally adjacent, and not backwards
-// A jump move of 2 squares is disabled if there aren't any opponent's piece(s) to capture
-function validMove(target, opponentPiece = false) {
+// A jump move of 2 squares is disabled by default if there aren't any opponent's piece(s) to capture
+function validMove(target, mandatoryJump = false) {
     const end = target.getAttribute("square-id");
     const start = parseInt(startPositionId);
 
@@ -13,43 +35,20 @@ function validMove(target, opponentPiece = false) {
     const colDiff = Math.abs(startCol - endCol);
 
     // Allow either a jump move (2 diagonal squares) or a regular move (1 diagonal square)
-    if (opponentPiece) {
+    if (mandatoryJump) {
         return rowDiff === 2 && colDiff === 2 && endRow < startRow;
     } else {
         return rowDiff === 1 && colDiff === 1 && endRow < startRow;
     }
-
 };
 
 
 // Function that checks whether there is an opponent piece in a square diagonally adjacent
-function getCapturedPiece(opponentTurn) {
+// If yes it is mandatory to make a jump move
+function checkMandatoryJump(opponentTurn) {
     const start = parseInt(startPositionId);
-    const row = Math.floor((63 - start) / width) + 1;
-    const col = start % width;
 
-    // Get the id for every diagonally adjacent square to the target, store it in the array
-    const squareIds = [];
-
-    // Check top-left
-    if (row > 1 && col > 0) {
-        squareIds.push((width - (row - 1)) * width + col - 1);
-    }
-
-    // Check top-right
-    if (row > 1 && col < 7) {
-        squareIds.push((width - (row - 1)) * width + col + 1);
-    }
-
-    // Check bottom-left
-    if (row < width && col > 0) {
-        squareIds.push((width - (row + 1)) * width + col - 1);
-    }
-
-    // Check bottom-right
-    if (row < width && col < 7) {
-        squareIds.push((width - (row + 1)) * width + col + 1);
-    }
+    const squareIds = checkDiagonals(start);
 
     console.log("Diagonally adjacent square id's:", squareIds);
 
@@ -58,9 +57,8 @@ function getCapturedPiece(opponentTurn) {
         const takenByOpponent = square.firstChild?.firstChild.classList.contains(opponentTurn);
 
         if (takenByOpponent) {
-            const opponentPiece = square;;
+            const opponentPiece = square;
 
-            // Check if the square beyond the opponent's piece is vacant
             if (opponentPiece) {
                 return opponentPiece;
             } else {
@@ -69,6 +67,9 @@ function getCapturedPiece(opponentTurn) {
         }
     };
 };
+
+
+
 
 
 
